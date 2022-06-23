9f891a8a91376e4c426e60b00229f20e54635232
Rong Wang <32773028+yulinrong@users.noreply.github.com>
2022-06-23 22:06:10 +0800

    Learn_english

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
 #你的python代码
+import fileinput
+import eng_to_ipa as p 
+import pyttsx3
+
+engine = pyttsx3.init()
+print("Please put your sentences in data.txt line by line.")
+for line in fileinput.input(files =('data.txt')):
+    if line[0] != '-':
+        sentence = line.split('.')[0]
+        phonetics = p.convert(sentence)
+        print(sentence)
+        print(phonetics)
+        engine.say(sentence)
+        engine.runAndWait()
+        a = input(".....................................................Press Enter")
+        continue
